387d07833470d3bd199917b39682bfd2c60e43d6
buibaanh2007-rgb <buibaanh2007@gmail.com>
2026-08-30 17:35:43 +0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -11,7 +11,7 @@ recognizer = sr.Recognizer()
 # Biến toàn cục quản lý trạng thái thức/ngủ và timeout 10 giây
 is_awake = False
 last_active_time = 0
-SLEEP_TIMEOUT = 10
+SLEEP_TIMEOUT = 15
 
 print("[Server] Đã sẵn sàng chạy theo yêu cầu mới!")
 
